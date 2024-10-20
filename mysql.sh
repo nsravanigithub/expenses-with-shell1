b892f010c0cb3847a7f3c4320099635f1e38ae42
nsravanigithub <nsravani1287@gmail.com>
2024-10-20 19:00:58 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -3,7 +3,8 @@
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-
+echo "Please enter DB Password:"
+read $(mysql_root_password)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -13,9 +14,9 @@ Validate ()
 {
     if [ $1 -eq 0 ]
     then
-    echo -e "Installation of $2 is:: $G Success $N"
+    echo -e "$2 is:: $G Success $N"
     else
-    echo -e "Installation of $2 is:: $R Failure $N"
+    echo -e "$2 is:: $R Failure $N"
     exit 1
     fi
 }
@@ -37,5 +38,16 @@ Validate $? "Enabling Mysql server"
 systemctl start mysqld &>>$LOGFILE
 Validate $? "Starting Mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#Validate $? "Setting Mysql server root password"
+
+#Below code will be useful for idempotent nature
+mysql -h db.devops4srav.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+
+if [ $? -eq 0 ]
+then
+mysql_secure_installation --set-root-pass mysql_root_password &>>$LOGFILE
 Validate $? "Setting Mysql server root password"
+else
+echo -e "DB root password already set...$Y Skipping $N"
+fi
