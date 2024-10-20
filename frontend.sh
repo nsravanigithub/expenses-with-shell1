5a54daab91bcb2aea99048552c5eaa95e4ea5c0b
nsravanigithub <nsravani1287@gmail.com>
2024-10-20 21:50:55 +0530

    some-message

--- frontend.sh
+++ frontend.sh
@@ -36,3 +36,16 @@ Validate $? "Enabling nginx"
 
 systemctl start nginx &>>$LOGFILE
 Validate $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+cp /home/ec2-user/expenses-with-shell1/expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl restart nginx &>>$LOGFILE
+Validate $? "Restarting nginx"
